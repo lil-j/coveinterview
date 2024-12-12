01de120f78b18289cedf67a25c29f77ac1e0f0f9
lil-j <jake@krunchy.net>
2024-12-12 14:55:47 -0800

    Add framer-motion and motion dependencies; update page layout and styles

--- app/page.js
+++ app/page.js
@@ -1,101 +1,143 @@
+"use client"
+
 import Image from "next/image";
+import {motion} from "framer-motion";
+import { useState } from "react";
+
+const spotlightedDishes = [
+  4, 5
+]
+const returnedMenu = [
+  {
+    type: "Category",
+    items: [
+      {
+        id: 1,
+        name: "Pasta",
+        linkedIds: [4],
+      },
+      {
+        id: 2, 
+        name: "Pizza",
+        linkedIds: [5]
+      }
+    ]
+  },
+  {
+    type: "Dish",
+    items: [
+      {
+        id: 4,
+        name: "Spaghetti",
+        price: 10.95,
+        linkedIds: [2, 3]
+      },
+      {
+        id: 5,
+        name: "Margherita Pizza",
+        price: 12.95,
+        linkedIds: [6, 7]
+      }
+    ]
+  },
+  {
+    type: "Option",
+    items: [
+      {
+        id: 2,
+        name: "Meatballs",
+        price: 1.00,
+      },
+      {
+        id: 3,
+        name: "Chicken",
+        price: 2.00,
+      },
+      {
+        id: 6,
+        name: "Extra Cheese",
+        price: 1.50,
+      },
+      {
+        id: 7,
+        name: "Pepperoni",
+        price: 2.00,
+      }
+    ]
+  }
+]
+
+
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState(returnedMenu[0].items[0].id);
+  function handleCategoryClick(id) {
+    setSelectedCategory(id);
+    // scroll to category
+    const category = document.getElementById(id);
+    category.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="max-w-3xl mx-auto mt-24">
+      
+      <div className="flex justify-between fixed top-0 w-full left-0 gap-4 bg-red-600 text-white px-4 py-4">
+      <h1 className="text-4xl font-boldtext-center">
+        Italian Restaurant
+      </h1>
+      <div className="flex gap-4">
+      {
+        returnedMenu[0].items.map((item) => {
+          return <div className="" key={item.id}>
+            <a className={`text-2xl font-medium cursor-pointer ${selectedCategory === item.id ? "" : "opacity-60"}`} onClick={
+              () => handleCategoryClick(item.id)
+            }>
+            {item.name}
+            {
+              selectedCategory === item.id && (
+                <motion.div 
+                layoutId="underline"
+                className="h-1 bg-white w-full">
+                </motion.div>
+              )
+            }
+            </a>
+            </div>
+        })
+      }
+      </div>
+      
+      </div>
+      {
+        returnedMenu[0].items.map((item) => {
+          return <div id={item.id} className="h-screen pt-32" key={item.id}>
+            <h2 className='text-2xl font-bold mb-2'>
+            {item.name}
+            </h2>
+            {
+              item.linkedIds.map((linkedId) => {
+                const linkedItem = returnedMenu[1].items.find((item) => item.id === linkedId);
+                return <div id={linkedId} key={linkedId}>
+                  <div className="flex justify-between">
+                  <h3 className="text-xl font-bold mb-1">{linkedItem.name}</h3>
+                  <p className="text-sm text-gray-500 mb-2">${linkedItem.price}</p>
+                    </div>
+                  {
+                    linkedItem.linkedIds.map((linkedId) => {
+                      const linkedItem = returnedMenu[2].items.find((item) => item.id === linkedId);
+                      return <div className="flex justify-between">
+                        <h4 className="text-sm font-medium mb-1">Add {linkedItem.name}</h4>
+                        <p className="text-xs text-gray-500 mb-2">${linkedItem.price}</p>
+                      </div>
+                    })
+                  }
+                  </div>
+              })
+            }
+          </div>
+        })
+      }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
